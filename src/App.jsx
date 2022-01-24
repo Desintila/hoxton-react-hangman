14f21da75d0d65c7d1e3786c3af42a29e53df182
Desintila <desintila.luzi@fshnstudent.info>
2022-01-24 23:30:16 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import { data } from './data'
+
+function App() {
+
+  const [word, setWord] = useState('')
+  const [guesses, setGuesses] = useState([])
+
+  useEffect(() => {
+    const randomWord = data[Math.floor(Math.random() * data.length)]
+    setWord(randomWord)
+  }, [])
+
+  useEffect(() => {
+    function handleOnClick(event) {
+      setGuesses(oldArray => [...oldArray, event.key])
+
+    }
+    document.addEventListener('keydown', handleOnClick)
+  }, [])
+
+
+
+  function getRightLetters() {
+    let letters = guesses
+    letters = letters.filter((letter) => {
+      return word.includes(letter)
+    })
+    return letters
+  }
+
+  function getWrongLetters() {
+    let letters = guesses
+    letters = letters.filter((letter) => {
+      return !word.includes(letter)
+    })
+    return letters
+  }
+
+
+
+
+
+  return (
+    <div className="App" >
+      <section className='hangman'>
+
+        {
+          word.split('').map((letter) => {
+            if (getRightLetters().includes(letter)) {
+              return (<span >{letter} </span>)
+            }
+            return (<span>_</span>)
+          })
+        }
+      </section>
+    </div >
+  )
+}
+
+export default App
